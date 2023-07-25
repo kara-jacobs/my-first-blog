0eac51b21f12ea22b6a85faa519badda4166238f
Kara Jacobs <kjacobs67@ivytech.edu>
2023-07-24 21:52:32 -0400

    new changes for Heroku that hopefully won't break anything

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,20 +79,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': 'djangogirls',
+        #'USER': 'name',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
 # Newly added
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 
 # Password validation
